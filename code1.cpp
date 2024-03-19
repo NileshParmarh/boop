4fc5da78a54a5bdee3cf3263f335c3c54769e538
Nilesh Parmar <162179682+NileshParmarh@users.noreply.github.com>
2024-03-19 23:55:28 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int num1 , num2;
+  
+  cout << "Enter first number:";
+  cin >> num1 ;
+  cout <<"Entar second numbar:";
+  cin >>num2 ;
+
+
+  
+  int add =  num1 + num2;
+  int multi = num1 * num2;
+  int sub = num1 - num2;
+  int div = num1 / num2;
+  
+  cout << "add:" << add << endl;
+  cout << "multi:" << multi << endl;
+  cout << "sub:" << sub << endl;
+  cout << "div:" << div << endl;
+  
+return 0;
+}
+
+
